2200cd1e2ac966db072a8af5372d17f0a77fe4b7
blinkfox <chenjiayin1990@163.com>
2015-10-17 23:27:07 +0800

    matery的ghost主题基本功能完成

---
+++ README.md
@@ -0,0 +1,1 @@
+这是一个漂亮、简洁、响应式并采用Material Design设计的Ghost博客主题。
---
+++ assets/js/index.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    /*菜单在各个屏幕大小下的切换*/
+    $('.button-collapse').sideNav();
+
+    /*切换搜索*/
+    $('#toggle-search').click(function() {
+        var search = $('#search');
+        search.is(":visible") ? search.slideUp() : search.slideDown(function() {
+            search.find('input').focus();
+        });
+        return false;
+    });
+
+    /*给文章详情内容图片增加light box*/
+    // $('#article-content img').addClass('materialboxed');
+    // $('.materialboxed').materialbox();
+
+    /*回到顶部*/
+    $('.scrollSpy').scrollSpy();
+
+    /*回到顶部按钮的显示和隐藏*/
+    $(window).scroll(function(event){
+        var t = $(window).scrollTop();
+        var ts = $('.top-scroll');
+        if (t < 50) {
+            ts.hide();
+        } else {
+            ts.show();
+        }
+    });
+
+});
